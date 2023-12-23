7d196dc550bb445dded7e6142b874a0da164e2da
Domingos Neto <domingosafneto@gmail.com>
2023-12-23 00:35:13 -0300

    criação da interface de usuário

---
+++ Data/SistemaTarefaDBContex.cs
@@ -0,0 +1,22 @@
+﻿using ApiTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTarefas.Data
+{
+    public class SistemaTarefaDBContex : DbContext
+    {
+        public SistemaTarefaDBContex(DbContextOptions<SistemaTarefaDBContex> options) : base(options) 
+        { 
+                
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; } // tabela de usuários a ser criada no migrations
+
+        public DbSet<TarefaModel> Tarefas { get; set; }  // ... de tarefas
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

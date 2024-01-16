4d5060de62934f4848e3d8836a5808a8f2f35daf
Domingos Neto <domingosafneto@gmail.com>
2024-01-16 13:57:39 -0300

    Crud de usuários

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -17,9 +17,44 @@ namespace ApiTarefas.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios()
         {
-            return Ok(); // 200 - sucesso
+            List<UsuarioModel> usuarios = await _usuarioRepositorio.BuscarTodosUsuarios();
+           
+            return Ok(usuarios); // 200 - sucesso
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UsuarioModel>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.BuscarPorId(id);
+           
+            return Ok(usuario); 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _usuarioRepositorio.Adicionar(usuarioModel);
+
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
+        {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _usuarioRepositorio.Atualizar(usuarioModel, id);
+
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+            bool apagado = await _usuarioRepositorio.Apagar(id);
+
+            return Ok(apagado);
         }
     }
 }
